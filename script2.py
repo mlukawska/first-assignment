5593b569c2a45bb953fd83ff4c0a6c0e9bc3c00c
Magdalena Lukawska <magdalena.lukawska96@gmail.com>
2020-09-26 14:43:06 +0200

    zestaw pierwszy - separator

--- script2.py
+++ script2.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv("train.tsv", sep = '\t',
                  names = ["Price", "Number_of_rooms", "Square_footage", "Floor_number", "Address", "Description"])
 
-df_description = pd.read_csv("description.csv", sep = ',')
+df_description = pd.read_csv("description.csv")
 
 #merging data frames
 new_df = df.merge(df_description, left_on='Floor_number', right_on='liczba', how='left')
